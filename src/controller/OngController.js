ed1105014b7c702b0bc3b74b2807bb5202b461bc
João Pedro <joaopedrobeckland@gmail.com>
2020-03-24 16:32:22 -0300

    Final Backend

---
+++ src/controller/OngController.js
@@ -0,0 +1,27 @@
+const connection = require('../database/connection')
+const crypto = require('crypto')
+
+module.exports = {
+  async index (Request, Response) {
+    const ongs = await connection('ongs').select('*')
+  
+    return Response.json(ongs)  
+  },
+
+  async create (Request, Response) {
+    const { name, email, whatsapp, city, uf } = Request.body;
+  
+    const id = crypto.randomBytes(4).toString('HEX')
+  
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
+  
+    return Response.json({ id })
+  }
+}
